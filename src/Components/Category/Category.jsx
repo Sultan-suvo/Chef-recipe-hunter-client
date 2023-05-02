1802546df430b0b7f0519914e43acdd77f7b564c
Sulthanmahmud Suvo <sulthansuvo72@gmailcom>
2023-05-03 00:02:15 +0600

    errorpage added

--- src/Components/Category/Category.jsx
+++ src/Components/Category/Category.jsx
@@ -4,10 +4,10 @@ const Category = ({ category }) => {
     const { image, name, id } = category
     return (
         <div>
-            <div className="card text-center mb-4">
+            <div className="card text-center mb-4 gap-2">
                 <img src={image} className="card-img-top mt-4 w-50 mx-auto rounded-circle" alt={name} />
                 <div className="card-body">
-                    <h2 className="card-title text-2xl">{name}</h2>
+                    <h2 className="card-title text-2xl font-bold">{name}</h2>
                 </div>
             </div>
         </div>
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,12 +3,14 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../Components/Home/Home";
 import Blog from "../Components/Blog/Blog";
+import ErrorPage from "../Components/ErrorPage/ErrorPage";
 
 
 const router = createBrowserRouter([
     {
         path: '/',
         element: <Main></Main>,
+        errorElement: <ErrorPage></ErrorPage>,
         children: [
             {
                 path: '/',
